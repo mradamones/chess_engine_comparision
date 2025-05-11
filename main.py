e5c59b3cb4b50709596932e82756c168e83133a4
szatk <szatkowski.a.ta@gmail.com>
2025-05-12 01:10:57 +0200

    Testing depth

--- main.py
+++ main.py
@@ -35,11 +35,11 @@ def nnue_move_depth(board):
 
 start = time.time()
 stockfish_path = "engines/stockfish.exe"
-# lczero_path = "engines/lc0/lc0.exe"
+lczero_path = "engines/lc0/lc0.exe"
 # stockfish_path = 'C:\\Users\\szatk\\AppData\\Roaming\\org.encroissant.app\\engines\\stockfish\\stockfish-windows-x86-64-avx2.exe'
 # lczero_path = 'D:\\Pobrane\\lc0\\lc0.exe'
 # stockfish_path = 'Y:\\chessengines\\stockfish\\stockfish-windows-x86-64-avx2.exe'
-lczero_path = 'Y:\\chessengines\\lc0_cuda\\lc0.exe'
+# lczero_path = 'Y:\\chessengines\\lc0_cuda\\lc0.exe'
 book_path = "./Komodo.bin"
 stockfish = chess.engine.SimpleEngine.popen_uci(stockfish_path, stderr=subprocess.DEVNULL)
 lczero = chess.engine.SimpleEngine.popen_uci(lczero_path, stderr=subprocess.DEVNULL)
